1ac99e8ce626857226578cf0287a2ac92fe0a683
sdeabhi <abhinandan.prajapati@kindasolutions.com>
2024-09-16 10:56:18 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'dist'
 };
 
 export default nextConfig;
-
-
